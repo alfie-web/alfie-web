f1eccb1829d2d8bfe778a9796531cda9bfcc24d2
alfie-web <alfie49@yandex.ru>
2020-04-14 14:29:55 +0000

    Организовал структуру хранения данных

---
+++ src/components/Weather/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export default function Weather({ item, activeWeather, setWeather }) {
+	const { title, icon, audio, video, _id } = item;
+	return (
+		<button 
+			className={ classNames('Button', { 'Button--active': activeWeather === _id }) } 
+			onClick={() => setWeather(audio, video, _id)}>
+			<img src={ icon } alt="Weather icon"/>
+			{ title }
+		</button>
+	)
+}
